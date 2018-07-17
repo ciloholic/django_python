7d6a7412ab027f852ada084675f19ec3e6d3e962
ciloholic <ciloholic0119@gmail.com>
2018-07-17 17:41:39 +0900

    テンプレート書き換え

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'admin/base_site.html')
--- django_python/settings.py
+++ django_python/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'django_python.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL='/login'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/login'
