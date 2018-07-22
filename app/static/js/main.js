eed2e3111623fcba4f13a7cc38e772180dd54261
ciloholic <ciloholic0119@gmail.com>
2018-07-22 20:58:11 +0900

    ・履歴のサンプルを追加 ・ローディングを追加

---
+++ app/static/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#download-button, #upload-button').click(function() {
+        $('#loading').toggleClass('show');
+    });
+});
--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 @login_required
 def index(request):
-    histories = ['update_file', 'update_file', 'download_file']
+    histories = [
+        {'timestamp': datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), 'name': 'update_file'},
+        {'timestamp': datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), 'name': 'update_file'},
+        {'timestamp': datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), 'name': 'download_file'},
+    ]
     context = {'title': 'トップ', 'site_title': 'ユーザサイト', 'histories': histories}
     return render(request, 'app/index.html', context)
